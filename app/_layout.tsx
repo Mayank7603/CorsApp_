ae5db0a234de94a668c0db2707b83b5828c63583
Mayank <sainimayank7603@gmail.com>
2024-09-01 06:06:01 +0530

    v1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,12 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
-      <Stack.Screen name="index" options={{headerShown:false}}/>
+      {/* Define screens in the stack */}
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="gender"  options={{ headerShown: false }} />
+      <Stack.Screen name="weight"  options={{ headerShown: false}} />
+      <Stack.Screen name="age"  options={{ headerShown: false }} />
     </Stack>
   );
 }
